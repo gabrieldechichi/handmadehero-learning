4300fb9d8597c63ff640b8fb70f034a9f6c359be
Gabriel Dechichi <gabrieldechichi@gmail.com>
2021-09-24 17:18:44 -0600

    Colorize build output

--- code/build.ps1
+++ code/build.ps1
@@ -1,3 +1,21 @@
+function Format-Color([hashtable] $Colors = @{}, [switch] $SimpleMatch) {
+    $lines = ($input | Out-String) -replace "`r", "" -split "`n"
+    foreach ($line in $lines) {
+        $color = ''
+        foreach ($pattern in $Colors.Keys) {
+            if (!$SimpleMatch -and $line -match $pattern) { $color = $Colors[$pattern] }
+            elseif ($SimpleMatch -and $line -like $pattern) { $color = $Colors[$pattern] }
+        }
+        if ($color) {
+            Write-Host -ForegroundColor $color $line
+        }
+        else {
+            Write-Host $line
+        }
+    }
+}
+
+
 & "$PSScriptRoot/../misc/config.ps1"
 
 $build_path = "$($project_vars.project_root)\build"
@@ -7,5 +25,5 @@ if (!(Test-Path $build_path)) {
 }
 
 pushd $build_path
-cl -FC -Zi '../code/win32/win32_handmade.cpp' user32.lib Gdi32.lib
+cl -FC -Zi '../code/win32/win32_handmade.cpp' user32.lib Gdi32.lib | Format-Color @{ 'warning' = 'Yellow'; 'error' = 'Red' }
 popd
